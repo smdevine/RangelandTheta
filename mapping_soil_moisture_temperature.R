2e70ae8ac70422ebbcce02ef2f077f07fcac76ba
smdevine <smdevine@ucdavis.edu>
2017-05-22 12:17:29 -0700

    Finished plotting of daily points values of soil moisture at both depth and created animations of these plots using the animation package

--- mapping_soil_moisture_temperature.R
+++ mapping_soil_moisture_temperature.R
@@ -2,11 +2,17 @@ library(rgdal)
 library(raster)
 library(gstat)
 library(spdep)
+library(extrafont)
+library(extrafontdb)
+loadfonts()
+library(animation)
 #define data summary directory (summaries produced by soil_moisture_processing.R)
 soil_VWCdata <- 'C:/Users/smdevine/Desktop/rangeland project/results/processed_soil_moisture/May2017/daily_by_location/VWC'
 soil_temperatureData <- 'C:/Users/smdevine/Desktop/rangeland project/results/processed_soil_moisture/May2017/daily_by_location/Temperature'
-#test for autocorrelation using spdep package (create function out of this later)
+dem_fineres <- 'C:/Users/smdevine/Desktop/rangeland project/DEMs_10cm'
+plot_results <- 'C:/Users/smdevine/Desktop/rangeland project/results/plots/May2017'
 
+#test for autocorrelation using spdep package (create function out of this later)
 data_dir <- soil_VWCdata #define working directory manually, either soil_VWCdata or soil_temperatureData
 setwd(data_dir)
 vwc_files <- list.files(pattern = glob2rx('*.csv')) #vwc_files can be taken to mean
@@ -40,7 +46,43 @@ for (j in 1:6) {
 }
 
 #make daily point plots of VWC and temperature data to put into a simple animation
+setwd(soil_VWCdata)
+vwc_files <- list.files(pattern = glob2rx('*.csv'))
+vwc_files #6 is median value at 7 cm depth; 5 is median value at 22 cm depth
+j <- 5
+vwc_data <- read.csv(vwc_files[j], stringsAsFactors = FALSE)
+coords <- vwc_data[ ,c('Est_10N', 'Nrt_10N')]
+vwc_data_sp <- SpatialPointsDataFrame(coords=coords, proj4string = crs('+proj=utm +zone=10 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0'), data=vwc_data)
+setwd(dem_fineres)
+dem_1m <- raster('camatta_Nov2016_1m_dsm.tif')
+dem_2m <- raster('camatta_Nov2016_2m_dsm.tif')
+dem_3m <- raster('camatta_Nov2016_3m_dsm.tif')
+hillshade_3m <- hillShade(terrain(dem_3m, opt='aspect'), terrain(dem_3m, opt='slope'), angle=45, direction=315)
+hillshade_2m <- hillShade(terrain(dem_2m, opt='aspect'), terrain(dem_2m, opt='slope'), angle=45, direction=315)
+hillshade_1m <- hillShade(terrain(dem_1m, opt='aspect'), terrain(dem_1m, opt='slope'), angle=45, direction=315)
 
+magfactor <- 15
+i <- 2
+
+for (i in 2:which(colnames(vwc_data)=='May_01_2017')) {
+  setwd(file.path(plot_results, '22cmdepth'))
+  png(file = paste(format(as.Date(names(vwc_data_sp)[i], format = '%b_%d_%Y'), '%Y%m%d'), '_daily_median_22cm.png', sep = ''), family = 'Book Antiqua', width = 700, height = 500, units = 'px', res=100)
+  par(mar=c(2, 2, 2, 3)) #does not affect right side I think because plotting a raster creates a space on the right even if if legend=FALSE
+  plot(hillshade_1m, main=paste(gsub('_', ' ', names(vwc_data_sp)[i]), 'soil moisture at 22 cm depth'), col=gray(30:80/100), legend=FALSE, axes=FALSE)
+  plot(dem_1m, col=terrain.colors(255, alpha=0.35), add=T)
+  #mtext(text='Elevation (m)', side=1, line=1, at=744800)
+  points(vwc_data_sp, cex=magfactor*vwc_data[,i], col='blue', pch=19)
+#text(vwc_data_sp, labels=vwc_data_sp$location, pos=1, cex=1.1, halo=T)
+  legend(x=744875, y=3931450, legend=c('0.10', '0.25', '0.40'), col='blue', pch=19, pt.cex=c(0.1*magfactor, 0.25*magfactor, 0.4*magfactor), x.intersp = 2, y.intersp = 1.9, bty="n")
+  text(x=744895, y=3931452, labels='soil VWC', font=2, offset=0)
+  dev.off()
+}
+
+#make an animation out of the soil moisture daily plots and forage data 
+setwd(file.path(plot_results, 'forage'))
+png_fnames <- list.files(pattern = glob2rx('*.png'))
+ani.options(convert="C:/PROGRA~1/ImageMagick/convert.exe", loop=1, interval=3, ani.width=700, ani.height=500) #see http://stackoverflow.com/questions/24904945/command-prompt-error-c-program-is-not-recognized-as-an-internal-or-external-c for why this path does not work -- "C:/Program Files/ImageMagick/convert.exe"
+im.convert(png_fnames, output = "Camatta_forage2017.gif")
 
 #playing around with semivariograms without invoking gstat, plotting all the pairs (120 total), as guided by lab 14 from Dr. Hijmans GEO200CN Spring 2016 course
 distance_pairs <- pointDistance(medianVWC7cm_sp, latlon=FALSE) #produces 2x + 16 the unique number of pairs
