b3b947701e93bf6b7122412d3992431721b3664e
smdevine <smdevine@ucdavis.edu>
2017-05-31 15:17:09 -0700

    Updated to make some new maps

--- general_mapping.R
+++ general_mapping.R
@@ -1,15 +1,20 @@
 library(raster)
 library(extrafont)
 library(extrafontdb)
+library(dismo)
 loadfonts()
 spatialDir <- 'C:/Users/smdevine/Desktop/rangeland project/soilmoisture/sensor_coordinates'
+additional_waypoints <- 'C:/Users/smdevine/Desktop/rangeland project/clip_plots/coordinates_waypoints'
+all_points <- 'C:/Users/smdevine/Desktop/rangeland project/clip_plots/results'
 terrainDir <- 'C:/Users/smdevine/Desktop/rangeland project/terrain_analysis_r'
 results <- 'C:/Users/smdevine/Desktop/rangeland project/results'
-#read-in coordinate data
 plotDir <- 'C:/Users/smdevine/Desktop/rangeland project/study_plots'
 elevDir <- 'C:/Users/smdevine/Desktop/rangeland project/elevation_NED10M_studysite/elevation'
 dem_fineres <- 'C:/Users/smdevine/Desktop/rangeland project/DEMs_10cm'
 plot_results <- 'C:/Users/smdevine/Desktop/rangeland project/results/plots/May2017'
+map_output <- 'C:/Users/smdevine/Desktop/rangeland project/maps'
+
+#read-in coordinate data
 setwd(spatialDir)
 sensor_coords <- read.csv('sensor_coords_UTM10N.csv', stringsAsFactors = FALSE)
 print(sensor_coords, digits=10) #default is 7, which could be changed under options(digits=...)
@@ -27,19 +32,54 @@ shapefile(x=sensor_pts, filename='5TM_sensor_locations_Camatta.shp')
 setwd(spatialDir)
 sensor_pts <- shapefile('5TM_sensor_locations_Camatta.shp')
 
+#read-in waypoints file
+setwd(additional_waypoints)
+#waypoints <- shapefile('')
+
 #plot aspect of the catchment from 1 m DEM
 setwd(dem_fineres)
 dem_1m <- raster('camatta_Nov2016_1m_dsm.tif')
 aspect_1m <- terrain(dem_1m, opt='aspect', unit = 'degrees')
 brks <- c(22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5)
 legend_key <- list(at=c(45, 90, 135, 180, 225, 270, 315, 355), labels=c('Northeast', 'East', 'Southeast', 'South', 'Southwest', 'West', 'Northwest', 'North'), line=2)
-setwd(plot_results)
-png(file = paste('Camatta_aspect_1mDEM.png', sep = ''), family = 'Book Antiqua', width = 700, height = 500, units = 'px', res=100)
+setwd(map_output)
+png(file = paste('Camatta_aspect_1mDEM.png', sep = ''), family = 'Book Antiqua', width = 1400, height = 1000, units = 'px', res=200)
+par(mar=c(4, 4, 3, 2))
+plot(aspect_1m, xlab='Longitude (UTM Zone 10N)', ylab='Latitude(UTM Zone 10N)', cex.axis=0.8, cex.lab=0.9, breaks=brks, legend.width = 1, legend.mar= 10, col=terrain.colors(8), axis.args=legend_key, legend.args=list(text='Aspect', side=4, line=0.5, cex=1)) #this is really calling the image.plot function; the par(mar..) is helping control where the legend bar is placed
+title(main='Sensor and waypoint locations and aspect of Camatta catchment', adj=0) #adj=1 creates right-justified text; default is 0.5 for centering title
+plot(all_pts_shp, pch=17, col='blue', add=T, cex=0.9)
+text(x=all_pts_shp, labels=all_pts_shp$location, pos=1, cex=0.5, offset=0.3, halo=T)
+dev.off()
+
+#plot aspect of the catchment from 1 m DEM with just sensor points 
+setwd(dem_fineres)
+dem_1m <- raster('camatta_Nov2016_1m_dsm.tif')
+aspect_1m <- terrain(dem_1m, opt='aspect', unit = 'degrees')
+brks <- c(22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5)
+legend_key <- list(at=c(45, 90, 135, 180, 225, 270, 315, 355), labels=c('Northeast', 'East', 'Southeast', 'South', 'Southwest', 'West', 'Northwest', 'North'), line=2)
+setwd(map_output)
+png(file = paste('Camatta_aspect_1mDEM_sensoronly.png', sep = ''), family = 'Book Antiqua', width = 700, height = 500, units = 'px', res=100)
 par(mar=c(4, 4, 3, 2))
 plot(aspect_1m, xlab='Longitude (UTM Zone 10N)', ylab='Latitude(UTM Zone 10N)', cex.axis=0.8, cex.lab=0.9, breaks=brks, legend.width = 1, legend.mar= 10, col=terrain.colors(8), axis.args=legend_key, legend.args=list(text='Aspect', side=4, line=0.5, cex=1)) #this is really calling the image.plot function; the par(mar..) is helping control where the legend bar is placed
 title(main='Soil moisture sensor locations and aspect of Camatta catchment', adj=0) #adj=1 creates right-justified text; default is 0.5 for centering title
 plot(sensor_pts, pch=17, col='blue', add=T)
-text(x=sensor_pts, labels=sensor_pts$dtlggr_, pos=1, cex=1.1, halo=T)
+text(x=sensor_pts, labels=sensor_pts$location, pos=1, cex=1.1, halo=T)
+dev.off()
+
+#general map of the catchment
+setwd(all_points)
+sensor_pts <- shapefile("sensor_forage2017.shp")
+waypoints <- shapefile("waypoint_forage2017.shp")
+all_pts_shp <- rbind(sensor_pts, waypoints)
+crs_mercator <- '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs'
+all_pts_shp_mercator <- spTransform(all_pts_shp, crs_mercator)
+camatta_aerial <- gmap(all_pts_shp_mercator, type = 'satellite', scale=2)
+setwd(map_output)
+jpeg(file = 'Camatta_sensors_and_waypoints_satellite.jpg', family = 'Book Antiqua', width = 2100, height = 1500, units = 'px', quality = 100, res = 250)
+#par(mar=c(4, 4, 3, 2))
+plot(camatta_aerial)
+plot(all_pts_shp_mercator, pch=17, col='yellow', add=T)
+text(x=all_pts_shp_mercator, labels=all_pts_shp_mercator$location, pos=1, offset=0.3, cex=0.5, halo=T)
 dev.off()
 
 #plot aspect of the catchment
@@ -52,14 +92,12 @@ brks <- c(22.5, 67.5, 112.5, 157.5, 202.5, 247.5, 292.5, 337.5)
 legend <- list(at=c(45, 90, 135, 180, 225, 270, 315, 355), labels=c('Northeast', 'East', 'Southeast', 'South', 'Southwest', 'West', 'Northwest', 'North'))
 plot(aspect_clip, main='Soil moisture sensor locations and aspect of Camatta catchment', breaks=brks, col=terrain.colors(8), axis.args=legend)
 
-
 #or, plot elevation
 setwd(elevDir)
 e <- raster('dem10m_combined.tif')
 plot(e)
 e_clip <- crop(e, camatta)
 plot(e_clip, main='Soil moisture sensor locations and elevation of Camatta catchment', col=terrain.colors(5))
-
 
 #and plot the sensor locations
 plot(sensor_pts, pch=17, col='blue', add=T)
