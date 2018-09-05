fe0edf296074d6e2a0d2dd9067bf717615bb55e1
smdevine <smdevine@ucdavis.edu>
2018-09-05 11:29:49 -0700

    and the saga continues

--- analysis_dissertation/soil_moisture_processing_2018.R
+++ analysis_dissertation/soil_moisture_processing_2018.R
@@ -507,18 +507,19 @@ dev.off()
   dev.off()
 
 #plotting 2017-2018 daily data by location x depth location from summaries produced above
-#this still needs work to deal with gap
-data_name <- 'VWC'
+#uses same object name (vwc_data_all) to plot either soil temperature or moisture
+data_name <- 'Temperature'
+varname <- 'T'
 depth <- '22'
 yr <- '2017_2018'
-vwc_data_all <- read.csv(file.path('C:/Users/smdevine/Desktop/rangeland project/results/processed_soil_moisture/Jul2018/daily_by_location', yr, data_name, paste0('MedianVWC_', depth, 'cm_dailymeans_by_location.csv')), stringsAsFactors = FALSE)
+vwc_data_all <- read.csv(file.path('C:/Users/smdevine/Desktop/rangeland project/results/processed_soil_moisture/Jul2018/daily_by_location', yr, data_name, paste0('Median', varname, '_', depth, 'cm_dailymeans_by_location.csv')), stringsAsFactors = FALSE)
 NA_start <- which(colnames(vwc_data_all)=='Jul_30_2017')
 NA_end <- which(colnames(vwc_data_all) == 'Nov_20_2017')
-#endcol <- ncol(vwc_data_all)
-#dates_all <- seq.Date(as.Date(colnames(vwc_data_all)[2], '%b_%d_%Y'), as.Date(colnames(vwc_data_all)[ncol(vwc_data_all)], '%b_%d_%Y'), by='day')
-#dates_all <- dates_all[-(which(dates_all=='2017-07-30'):which(dates_all=='2017-11-20'))]
-#weeks_all <- seq.Date(as.Date(colnames(vwc_data_all)[2], '%b_%d_%Y'), as.Date(colnames(vwc_data_all)[ncol(vwc_data_all)], '%b_%d_%Y'), by='week')
-#weeks_all <- weeks_all[-(which(weeks_all=='2017-07-15'):which(weeks_all=='2017-11-25'))]
+endcol <- ncol(vwc_data_all)
+dates_all <- seq.Date(as.Date(colnames(vwc_data_all)[2], '%b_%d_%Y'), as.Date(colnames(vwc_data_all)[ncol(vwc_data_all)], '%b_%d_%Y'), by='day')
+dates_all <- dates_all[-(which(dates_all=='2017-07-30'):which(dates_all=='2017-11-20'))]
+weeks_all <- seq.Date(as.Date(colnames(vwc_data_all)[2], '%b_%d_%Y'), as.Date(colnames(vwc_data_all)[ncol(vwc_data_all)], '%b_%d_%Y'), by='week')
+weeks_all <- weeks_all[-(which(weeks_all=='2017-07-15'):which(weeks_all=='2017-11-25'))]
 vwc_data_all_temp <- vwc_data_all[ ,-(NA_start:NA_end)]
 for (i in 1:nrow(vwc_data_all_temp)) {
   plot(as.factor(dates_all), vwc_data_all_temp[i, 2:ncol(vwc_data_all_temp)], type='b', xlab='Date', ylab='Daily Median VWC at 7 cm (mean of 2 sensors)', xaxt='n', main=paste('Location', vwc_data_all_temp$location[i])) #, ',', vwc_data_all_temp$aspect_cardinal[i], 'aspect'))
@@ -531,6 +532,7 @@ a <- match(as.Date(precip_data$Date, '%m/%d/%Y'), dates_all)
 precip_data_merge <- precip_data[-which(is.na(a)), ]
 precip_days_dummy <- match(as.Date(precip_data_merge$Date, '%m/%d/%Y'), dates_all)
 
+#plotting for soil moisture
 png(file = file.path(results, 'figures', paste0('WY2017_2018soilmoisture.', depth, 'cm.v2.png')), family = 'Book Antiqua', width = 1200, height = 400, units = 'px', res=100)
 par(mar=c(2, 4, 2, 4))
 for (i in 1:nrow(vwc_data_all_temp)) {
@@ -546,14 +548,25 @@ date_indices <- c(which(dates_all=='2016-12-01'), which(dates_all=='2017-01-01')
 axis(side = 1, at=date_indices, labels = format(dates_all[date_indices], '%m/%d/%y'))
 #add precip
 axis(side = 4, at = c(0.1, 0.2, 0.3, 0.4), labels = c('10', '20', '30', '40'))
-mtext("mm precipitation", side=4, line=2.5)
+mtext("mm precipitation per day", side=4, line=2.5)
 lines(precip_days_dummy, precip_data_merge$Rainfall..mm. / 100, type='s', col='blue')
 dev.off()
 
-
-
-dates_all
-
+#plot for soil temperature
+png(file = file.path(results, 'figures', paste0('WY2017_2018soiltemperature.', depth, 'cm.v2.png')), family = 'Book Antiqua', width = 1200, height = 400, units = 'px', res=100)
+par(mar=c(2, 4, 2, 4))
+for (i in 1:nrow(vwc_data_all_temp)) {
+  if (i == 1) {
+    plot(x=as.factor(dates_all), y=vwc_data_all_temp[i, 2:ncol(vwc_data_all_temp)], type='l', xaxt='n', col=i+1, ylim=c(0, 40), xlim=c(15, length(dates_all) - 15), xlab = "", ylab='deg C', main = paste('WY2017-2018 soil temperature,', depth, 'cm depth, Camatta catchment'))
+    #text(x=dates_all[length(dates_all)-50], y=vwc_data_all_temp[i, ncol(vwc_data_all_temp)-50], labels=vwc_data_all_temp[i, 1], cex=0.7)
+  } else {
+    lines.default(as.factor(dates_all), vwc_data_all_temp[i, 2:ncol(vwc_data_all_temp)], xaxt='n', col=i+1)
+    #text(x=dates_all[length(dates_all)- 50], y=vwc_data_all_temp[i, (ncol(vwc_data_all_temp)- 50)], labels=vwc_data_all_temp[i, 1], cex=0.7)
+  }
+}
+date_indices <- c(which(dates_all=='2016-12-01'), which(dates_all=='2017-01-01'), which(dates_all=='2017-02-01'), which(dates_all=='2017-03-01'), which(dates_all=='2017-04-01'), which(dates_all=='2017-05-01'), which(dates_all=='2017-06-01'), which(dates_all=='2017-07-01'), which(dates_all=='2017-08-01'), which(dates_all=='2017-09-01'), which(dates_all=='2017-10-01'), which(dates_all=='2017-11-01'), which(dates_all=='2017-12-01'), which(dates_all=='2018-01-01'), which(dates_all=='2018-02-01'), which(dates_all=='2018-03-01'), which(dates_all=='2018-04-01'), which(dates_all=='2018-05-01'), which(dates_all=='2018-06-01'), which(dates_all=='2018-07-01'))
+axis(side = 1, at=date_indices, labels = format(dates_all[date_indices], '%m/%d/%y'))
+dev.off()
 
 #normalize both years vwc_data
 vwc_data_normalized_all <- vwc_data_all
@@ -576,10 +589,39 @@ abline(0, 0, lty = 2)
 forage_data <- read.csv(file.path(forageDir, 'summaries', 'forage2017_2018.by.sensor.csv'), stringsAsFactors=FALSE)
 #combine with sensor characteristics
 forage_terrain <- merge(forage_data, terrain_chars, by='location')
+
+
+#relate to Feb 2017 forage data
+summary(lm(clp021517 ~ elevation, data = forage_terrain)) #r^2=0.17
+summary(lm(clp021517 ~ aspect, data = forage_terrain))
+png(file.path(results, 'figures', 'aspect.vs.WY2017Feb.forage.png', sep = ''), family = 'Book Antiqua', width = 800, height = 600, units = 'px', res=100)
+par(mar=c(4.5, 4.5, 2, 2))
+plot(forage_terrain$aspect, forage_terrain$clp021517, ylab='Feb 15, 2017 biomass (kg / ha)', xlab='aspect (degrees)', main='Relationship between aspect and 2017 standing forage in February, Camatta catchment', cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.1)
+text(x=forage_terrain$aspect, y=forage_terrain$clp021517, labels = forage_terrain$location, pos = 1, offset = 0.3)
+abline(lm(clp021517 ~ aspect, data = forage_terrain), lty=2)
+dev.off()
+
+summary(lm(clp021517 ~ slope, data = forage_terrain))
+summary(lm(clp021517 ~ curvature_mean, data = forage_terrain))
+summary(lm(clp021517 ~ curvature_plan, data = forage_terrain)) #r2=0.28
+summary(lm(clp021517 ~ curvature_profile, data = forage_terrain))
+summary(lm(clp021517 ~ CTI, data = forage_terrain)) #r^2=0.21
+summary(lm(clp021517 ~ CTI + curvature_plan + aspect, data = forage_terrain)) #r^2 = 0.38
+summary(lm(clp021517 ~ CTI + aspect, data = forage_terrain)) #both sig, r^2=0.40
+summary(lm(clp021517 ~ curvature_plan + aspect, data = forage_terrain)) #r^2 = 0.32
+
+
+#relate to March 14, 2017 forage data
 summary(lm(clp031417 ~ elevation, data = forage_terrain))
 summary(lm(clp031417 ~ aspect, data = forage_terrain)) #best model: r2=0.39
-plot(forage_terrain$aspect, forage_terrain$clp031417)
+#make an aspect plot vs. March 14, 2017 forage data
+png(file.path(results, 'figures', 'aspect.vs.WY2017Mar.forage.png', sep = ''), family = 'Book Antiqua', width = 800, height = 600, units = 'px', res=100)
+par(mar=c(4.5, 4.5, 2, 2))
+plot(forage_terrain$aspect, forage_terrain$clp031417, ylab='Mar 14, 2017 biomass (kg / ha)', xlab='aspect (degrees)', main='Relationship between aspect and 2017 standing forage in March, Camatta catchment', cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.1)
+text(x=forage_terrain$aspect, y=forage_terrain$clp031417, labels = forage_terrain$location, pos = 1, offset = 0.3)
 abline(lm(clp031417 ~ aspect, data = forage_terrain), lty=2)
+dev.off()
+
 summary(lm(clp031417 ~ slope, data = forage_terrain))
 summary(lm(clp031417 ~ curvature_mean, data = forage_terrain))
 summary(lm(clp031417 ~ curvature_plan, data = forage_terrain)) #r2=0.34
@@ -604,8 +646,14 @@ summary(lm(forage_terrain$clp031417 ~ forage_terrain$aspect + forage_terrain$CTI
 #and look at correlation with Apr biomass
 summary(lm(clp041017 ~ elevation, data = forage_terrain))
 summary(lm(clp041017 ~ aspect, data = forage_terrain)) #no longer significant
-plot(forage_terrain$aspect, forage_terrain$clp041017)
+
+png(file.path(results, 'figures', 'aspect.vs.WY2017Apr.forage.png', sep = ''), family = 'Book Antiqua', width = 800, height = 600, units = 'px', res=100)
+par(mar=c(4.5, 4.5, 2, 2))
+plot(forage_terrain$aspect, forage_terrain$clp041017, ylab='Apr 10, 2017 biomass (kg / ha)', xlab='aspect (degrees)', main='Relationship between aspect and 2017 standing forage in April, Camatta catchment', cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.1)
 abline(lm(clp041017 ~ aspect, data = forage_terrain), lty=2)
+text(x=forage_terrain$aspect, y=forage_terrain$clp041017, labels = forage_terrain$location, pos = 1, offset = 0.3)
+dev.off()
+
 summary(lm(clp041017 ~ slope, data = forage_terrain))
 summary(lm(clp041017 ~ curvature_mean, data = forage_terrain))
 summary(lm(clp041017 ~ CTI, data = forage_terrain)) #r2=0.23
@@ -624,8 +672,14 @@ summary(lm(forage_terrain$clp041017 ~ forage_terrain$curvature_plan + vwc_data_n
 #and look at correlation with May biomass
 summary(lm(clp050117 ~ elevation, data = forage_terrain))
 summary(lm(clp050117 ~ aspect, data = forage_terrain))
-plot(forage_terrain$aspect, forage_terrain$clp050117)
+
+png(file.path(results, 'figures', 'aspect.vs.WY2017May.forage.png', sep = ''), family = 'Book Antiqua', width = 800, height = 600, units = 'px', res=100)
+par(mar=c(4.5, 4.5, 2, 2))
+plot(forage_terrain$aspect, forage_terrain$clp050117, ylab='May 1, 2017 biomass (kg / ha)', xlab='aspect (degrees)', main='Relationship between aspect and 2017 standing forage in May, Camatta catchment', cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.1)
 abline(lm(clp050117 ~ aspect, data = forage_terrain), lty=2)
+text(x=forage_terrain$aspect, y=forage_terrain$clp050117, labels = forage_terrain$location, pos = 1, offset = 0.3)
+dev.off()
+
 summary(lm(clp050117 ~ slope, data = forage_terrain))
 summary(lm(clp050117 ~ curvature_mean, data = forage_terrain))
 summary(lm(clp050117 ~ curvature_plan, data = forage_terrain))
@@ -641,6 +695,10 @@ summary(lm(clp050117 ~ aspect + curvature_mean, data = forage_terrain))
 summary(lm(clp050117 ~ aspect + CTI, data = forage_terrain)) #0.18 r^2
 lm_May2017_CTI <- lm(clp050117 ~ CTI, data = forage_terrain)
 lm_May2017_CTI_curv <- lm(clp050117 ~ CTI + curvature_mean, data = forage_terrain)
+
+
+#analyze Feb 2018 biomass
+
 
 #analyze March 2018 biomass
 summary(lm(clp032218 ~ elevation, data = forage_terrain))
