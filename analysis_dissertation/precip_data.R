70334eac7df4771971e7cbc4da00ff1b3529d417
smdevine <smdevine@ucdavis.edu>
2018-12-01 17:33:40 -0800

    includes a look at a file sent from Grace which was compilation of Camatta precip data and nearby weather station from 2001-2014

--- analysis_dissertation/precip_data.R
+++ analysis_dissertation/precip_data.R
@@ -4,17 +4,37 @@ loadfonts()
 results <- 'C:/Users/smdevine/Desktop/rangeland project/results'
 climateDir <- 'C:/Users/smdevine/Desktop/rangeland project/climate_data'
 PRISM_precip <- read.csv(file.path(climateDir, 'PRISM_ppt_provisional_Camatta.csv'), stringsAsFactors = FALSE)
-precip_data <- read.csv(file.path('C:/Users/smdevine/Desktop/rangeland project/climate_data/Camatta_precip_WY2017_2018.csv'), stringsAsFactors = FALSE)
+precip_data <- read.csv(file.path('C:/Users/smdevine/Desktop/rangeland project/climate_data/Camatta_precip_WY2017_2018.csv'), stringsAsFactors = FALSE) #this is from "north" gauge; "top" gauge was incomplete in 2017, pending data from Grace
+precip_data_v2 <- read.csv(file.path(climateDir, 'Camatta_precip_2001_2018by.month.csv'), stringsAsFactors = FALSE) #sent via email from Grace 11/21/18; 3 years of Camatta data plus data from nearby gauge 2000-2014
 #WY 2018 was 10/1/2017-4/25/2018 per weather station
 #WY 2017 was 10/27/2017-4/20/2017 per weather station and so missed one mid-October precip event which was before soil moisture sensors were installed
 #there were no late April rains in either year
+colnames(precip_data_v2)
+colnames(precip_data_v2)[3:ncol(precip_data_v2)] <- paste0('WY.', 2001:2018) 
+tapply(precip_data_v2$WY.2017, precip_data_v2$Month, mean)
+tapply(precip_data_v2$WY.2017, precip_data_v2$Month, max)
+tapply(precip_data_v2$WY.2017, precip_data_v2$Month, min)
+precip_summary_allyrs <- do.call(cbind, lapply(precip_data_v2[,3:ncol(precip_data_v2)], function(x) tapply(x, precip_data_v2$Month, function(a) 25.4 * mean(a))))
+apply(precip_summary_allyrs, 2, sum)
+mean(apply(precip_summary_allyrs, 2, sum)) #212.9743
+precip_summary <- as.data.frame(t(data.frame(WY.2017=tapply(precip_data_v2$WY.2017, precip_data_v2$Month, function(x) 25.4 * mean(x)), WY.2018=tapply(precip_data_v2$WY.2018, precip_data_v2$Month, function(x) 25.4 * mean(x))))) #c('Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr')
+precip_summary
+precip_summary_GS <- precip_summary[ ,c('Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr')]
+precip_summary_GS$TOTAL <- apply(precip_summary_GS, 1, sum)
+precip_summary_GS
+tiff(file = file.path(results, 'figures', 'finals', 'precip_summary',  'WY2017_2018_precip_11.21.18.tif'), family = 'Times New Roman', pointsize = 11, width = 6.5, height = 4.5, units = 'in', res=150)
+par(mar=c(3, 5, 1, 1))
+barplot(as.matrix(precip_summary_GS), beside = TRUE, col=c('blue', 'red3'), ylab = 'Precipitation (mm)', legend.text = c('WY2017 (wet)', 'WY2018 (dry)'), cex.axis = 1, cex.names = 1, cex.lab = 1, args.legend = list(x="topleft", inset=0.1, cex=1))
+dev.off()
+
+#working with old precip_file
 precip_data$Date
 precip_data$Date <- as.Date(precip_data$Date, format = '%m/%e/%Y')
 precip_data$Month <- format.Date(precip_data$Date, '%b')
 precip_data$Year <- format.Date(precip_data$Date, '%Y')
 precip_data$WY <- ifelse(precip_data$Date < as.Date('2017-07-01'), 2017, 2018)
 precip_summary <- as.data.frame(tapply(precip_data$Rainfall..mm., list(precip_data$WY, precip_data$Month), sum))
-Oct_15_16_2016 <- 25.4 * (0.13 + 0.02) #from Paso Robles station
+Oct_15_16_2016 <- 2 #from 'top' rain gauge at site #4 mm recorded by NWS: 25.4 * (0.13 + 0.02) from Paso Robles station
 names(precip_summary)
 precip_summary <- precip_summary[ ,c('Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr')]
 precip_summary$Oct[1] <- precip_summary$Oct[1] + Oct_15_16_2016
@@ -25,6 +45,10 @@ par(mar=c(3, 5, 1, 1))
 barplot(as.matrix(precip_summary), beside = TRUE, col=c('blue', 'red3'), ylab = 'Precipitation (mm)', legend.text = c('WY2017 (wet)', 'WY2018 (dry)'), cex.axis = 1, cex.names = 1, cex.lab = 1, args.legend = list(x="topleft", inset=0.1, cex=1))
 dev.off()
 precip_summary
+precip_summary_GS$TOTAL[1] / 526.5 # ratio to PRISM estimate for WY 2017
+# 0.5321216
+precip_summary_GS$TOTAL[2] / 226.5 # ratio to PRISM estimate for WY 2018
+# 0.5427638
 
 #PRISM precip
 head(PRISM_precip)
@@ -34,8 +58,12 @@ PRISM_precip$Date_false <- as.Date(x=PRISM_precip$Date_false, format = '%Y-%m-%d
 PRISM_precip$Month <- as.integer(format.Date(PRISM_precip$Date_false, '%m'))
 PRISM_precip$Year <- as.integer(format.Date(PRISM_precip$Date_false, '%Y'))
 PRISM_precip$WY <- ifelse(PRISM_precip$Month <= 6, PRISM_precip$Year, PRISM_precip$Year+1)
+tapply(PRISM_precip$tmean..degrees.F.[PRISM_precip$Year %in% 1980:2010], PRISM_precip$Month[PRISM_precip$Year %in% 1980:2010], mean)
+(46.16452 - 32) * (5/9)
+(74.22258 - 32) * (5/9)
 WY_summary <- data.frame(year=unique(PRISM_precip$WY), precip_WY_mm=as.numeric(tapply(PRISM_precip$ppt..inches., PRISM_precip$WY, function(x) sum(x) * 25.4)))
 mean(WY_summary$precip_WY_mm[WY_summary$year %in% 2001:2018])
+mean(WY_summary$precip_WY_mm[WY_summary$year %in% 1980:2010])
 plot(WY_summary$year[-c(1,nrow(WY_summary))], WY_summary$precip_WY_mm[-c(1,nrow(WY_summary))], type = 'b')
 abline(h=mean(WY_summary$precip_WY_mm), lty=2, col='red')
 PRISM_precip_GS <- PRISM_precip[PRISM_precip$Month <= 5 | PRISM_precip$Month >= 10, ]
